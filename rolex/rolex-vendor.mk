8ace06d2f25f7a82c4a69c58b77046b1021a09a2
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-12-26 22:14:57 +0530

    rolex: update blobs

--- rolex/rolex-vendor.mk
+++ rolex/rolex-vendor.mk
@@ -38,10 +38,10 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/rolex/proprietary/bin/msm_irqbalance:system/bin/msm_irqbalance \
     vendor/xiaomi/rolex/proprietary/bin/netmgrd:system/bin/netmgrd \
     vendor/xiaomi/rolex/proprietary/bin/pm-proxy:system/bin/pm-proxy \
-    vendor/xiaomi/rolex/proprietary/bin/qti:system/bin/qti \
     vendor/xiaomi/rolex/proprietary/bin/pm-service:system/bin/pm-service \
     vendor/xiaomi/rolex/proprietary/bin/port-bridge:system/bin/port-bridge \
     vendor/xiaomi/rolex/proprietary/bin/qseecomd:system/bin/qseecomd \
+    vendor/xiaomi/rolex/proprietary/bin/qti:system/bin/qti \
     vendor/xiaomi/rolex/proprietary/bin/radish:system/bin/radish \
     vendor/xiaomi/rolex/proprietary/bin/rmt_storage:system/bin/rmt_storage \
     vendor/xiaomi/rolex/proprietary/bin/sensors.qcom:system/bin/sensors.qcom \
@@ -227,8 +227,8 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/rolex/proprietary/lib/libOmxSwVencMpeg4.so:system/lib/libOmxSwVencMpeg4.so \
     vendor/xiaomi/rolex/proprietary/lib/libOmxVdec.so:system/lib/libOmxVdec.so \
     vendor/xiaomi/rolex/proprietary/lib/libOmxVpp.so:system/lib/libOmxVpp.so \
-    vendor/xiaomi/rolex/proprietary/lib/libRecoFace.so:system/lib/libRecoFace.so \
     vendor/xiaomi/rolex/proprietary/lib/libqomx_core.so:system/lib/libqomx_core.so \
+    vendor/xiaomi/rolex/proprietary/lib/libRecoFace.so:system/lib/libRecoFace.so \
     vendor/xiaomi/rolex/proprietary/lib/libshims_camera.so:system/lib/libshims_camera.so \
     vendor/xiaomi/rolex/proprietary/lib/libstagefright_omx.so:system/lib/libstagefright_omx.so \
     vendor/xiaomi/rolex/proprietary/lib/libstagefright_soft_flacdec.so:system/lib/libstagefright_soft_flacdec.so \
@@ -237,12 +237,13 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/rolex/proprietary/lib/modules/pronto/pronto_wlan.ko:system/lib/modules/pronto/pronto_wlan.ko \
     vendor/xiaomi/rolex/proprietary/lib/soundfx/libdirac.so:system/lib/soundfx/libdirac.so \
     vendor/xiaomi/rolex/proprietary/lib64/hw/consumerir.default.so:system/lib64/hw/consumerir.default.so \
-    vendor/xiaomi/rolex/proprietary/lib64/libOmxSwVdec.so:system/lib64/libOmxSwVdec.so \
-    vendor/xiaomi/rolex/proprietary/lib64/libOmxSwVencMpeg4.so:system/lib64/libOmxSwVencMpeg4.so \
-    vendor/xiaomi/rolex/proprietary/lib64/libOmxVdec.so:system/lib64/libOmxVdec.so \
     vendor/xiaomi/rolex/proprietary/lib64/libloc_api_v02.so:system/lib64/libloc_api_v02.so \
     vendor/xiaomi/rolex/proprietary/lib64/libloc_ds_api.so:system/lib64/libloc_ds_api.so \
     vendor/xiaomi/rolex/proprietary/lib64/libmm-omxcore.so:system/lib64/libmm-omxcore.so \
+    vendor/xiaomi/rolex/proprietary/lib64/libOmxSwVdec.so:system/lib64/libOmxSwVdec.so \
+    vendor/xiaomi/rolex/proprietary/lib64/libOmxSwVencMpeg4.so:system/lib64/libOmxSwVencMpeg4.so \
+    vendor/xiaomi/rolex/proprietary/lib64/libOmxVdec.so:system/lib64/libOmxVdec.so \
+    vendor/xiaomi/rolex/proprietary/lib64/libOmxVpp.so:system/lib64/libOmxVpp.so \
     vendor/xiaomi/rolex/proprietary/lib64/libstagefright_omx.so:system/lib64/libstagefright_omx.so \
     vendor/xiaomi/rolex/proprietary/lib64/libstagefright_soft_flacdec.so:system/lib64/libstagefright_soft_flacdec.so \
     vendor/xiaomi/rolex/proprietary/lib64/libvpplibrary.so:system/lib64/libvpplibrary.so \
@@ -252,7 +253,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/rolex/proprietary/vendor/bin/qti:system/vendor/bin/qti \
     vendor/xiaomi/rolex/proprietary/vendor/bin/slim_daemon:system/vendor/bin/slim_daemon \
     vendor/xiaomi/rolex/proprietary/vendor/bin/thermal-engine:system/vendor/bin/thermal-engine \
-    vendor/xiaomi/rolex/proprietary/vendor/etc/diracmobile.config:system/vendor/etc/diracmobile.config \
     vendor/xiaomi/rolex/proprietary/vendor/lib/egl/eglsubAndroid.so:system/vendor/lib/egl/eglsubAndroid.so \
     vendor/xiaomi/rolex/proprietary/vendor/lib/egl/eglSubDriverAndroid.so:system/vendor/lib/egl/eglSubDriverAndroid.so \
     vendor/xiaomi/rolex/proprietary/vendor/lib/egl/libEGL_adreno.so:system/vendor/lib/egl/libEGL_adreno.so \
